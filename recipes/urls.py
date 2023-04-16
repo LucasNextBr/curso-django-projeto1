73ce9f86f95c362af12b380615fa1111f9ccba6a
Lucas Mateus <lucasmts.sales@gmail.com.br>
2023-04-16 12:03:07 -0300

    Remove of examples

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,8 @@
-from django.http import HttpResponse
 from django.urls import include, path
 
-from recipes.views import contato, home, sobre
+from recipes.views import home
 
 urlpatterns = [
     path(route='',    view=home),
-    path(route='contato/', view=contato),
-    path(route='sobre/',   view=sobre),
+
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -6,11 +5,3 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/home.html', context={'name': 'Lucas Mateus Sousa Sales'})
-
-
-def contato(request):
-    return HttpResponse('contato')
-
-
-def sobre(request):
-    return HttpResponse('Sobre')
